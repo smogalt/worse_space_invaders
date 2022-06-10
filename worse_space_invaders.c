e8c27cb33c359265b7c9e6dedff4eda4f71de7d4
scint <fooffed@protonmail.com>
2022-06-10 18:48:29 -0400

    Collisions added and multiple enemies now working

--- worse_space_invaders.c
+++ worse_space_invaders.c
@@ -8,7 +8,8 @@
 int max_box_coords[2] = {21, 41};  // X and Y, respectively
 int player_coords[2] = {39, 9};    // ^^^^^^^^^^^^^^^^^^^^^
 int enemy_y = 2;
-int enemy_x;
+int enemy_x[5];
+int x_index;
 
 
 /* Function Prototypes */
@@ -30,6 +31,16 @@ int main() {
 
 	/* Main game loop */
 	while(1) {
+		
+		/* Collisions */
+		for (x_index = 0; x_index <= 4; x_index++) {
+			if (enemy_x[x_index] == player_coords[1] && enemy_y == player_coords[0]) {
+				endwin();
+				return(0);
+
+			}
+		}
+		
 		/* Player Controls */
 		char input_char = getch();
 		switch(input_char) {
@@ -67,15 +78,9 @@ int main() {
 		/* Enemy movement */		
 		check_enemy_y(enemy_y);
 		enemy_y++;
-		mvaddch(enemy_y, enemy_x, 'X');
-		mvaddch(enemy_y, rand() % 21, 'X');		
-
-		/* Collisions */
-		if (enemy_y == player_coords[1] && enemy_x == player_coords[0]) {
-			endwin();
-			return(0);
-
-		}
+		for (int index = 0; index <= 4; index++)
+			mvaddch(enemy_y, enemy_x[index], 'X');
+		
 
 
 		/* Draw box */
@@ -85,18 +90,20 @@ int main() {
 		refresh();
 	}	
 	endwin();
-
+	for (int index = 0; index <= 4; index++)
+		printf("%d", enemy_x[index]);
 	return(0);
 }
 
 
 /* Function definitions */
-void check_enemy_y(int y) {
+void check_enemy_y(int y) {	// To reset and assign X values to enemies
 	switch (y) {
 		case 40 :
 			enemy_y = 2;
-			enemy_x = rand() % 21;
-			break;
+			for (int index = 0; index <= 4; index++)
+				enemy_x[index] = rand() % 21;
+				break;
 		
 		case 1 :
 			enemy_y = 2;
